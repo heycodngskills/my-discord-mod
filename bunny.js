070c54580a05c0afd9b8715ef0dfb3f1321c18fa
heycodngskills <heycodngskills@users.noreply.github.com>
2026-06-25 05:15:49 +0000

    deploy: heycodngskills/discord-ipa-builder@84fdfcef649ca56eea3252832e0fda919decf815

--- bunny.js
+++ bunny.js
@@ -4653,7 +4653,7 @@
       init_logger();
       init_toasts();
       import_react_native5 = __toESM(require_react_native());
-      versionHash = "3b3af38-local";
+      versionHash = "a38c80a-local";
     }
   });
 
@@ -15185,7 +15185,7 @@
             uri: pyoncord_default
           },
           render: () => Promise.resolve().then(() => (init_General(), General_exports)),
-          useTrailing: () => `(${"3b3af38-local"})`
+          useTrailing: () => `(${"a38c80a-local"})`
         },
         {
           key: "BUNNY_PLUGINS",
@@ -15682,7 +15682,7 @@
         alert([
           "Failed to load Bunny!\n",
           `Build Number: ${ClientInfoManager.Build}`,
-          `Bunny: ${"3b3af38-local"}`,
+          `Bunny: ${"a38c80a-local"}`,
           stack || e?.toString?.()
         ].join("\n"));
       }
